9ac7b702ebb234f9661bad85ef2ca7c8fa1cbfba
ZucchiniZe <me@alexb.ninja>
2015-08-26 00:29:06 -0700

    index description

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var base = "http://zucchinize.epsilon.bysh.me/notes/";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/:category/:name', function(req, res) {
